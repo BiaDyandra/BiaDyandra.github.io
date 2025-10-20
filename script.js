242c2719c6875d1074d6c7e000bb6c797b5bcbf6
Bianca Dyandra Ribeiro Gomes De Farias <140829861+BiaDyandra@users.noreply.github.com>
2025-10-20 12:20:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('toggleTheme');
+button.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  const darkMode = document.body.classList.contains('dark');
+  button.textContent = darkMode ? '☀️ Modo Claro' : '🌙 Modo Escuro';
+});
+
